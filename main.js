77d8e4be2dbca2b02ad0da3c555236ed995f2d2e
Matteo Marguerettaz <matteo.marguerettaz@gmail.com>
2024-09-14 18:25:19 +0900

    First lesson: create and mount the app, add and use properties

--- main.js
+++ main.js
@@ -1,1 +1,9 @@
-const product = 'Socks'
+const app = Vue.createApp({
+    //data: function(){ // same as
+    data(){ //ES6 Shorthand
+        return {
+            product: 'Boots',
+            description: 'Agility boots that increase your walking speed',
+        }
+    } 
+})
